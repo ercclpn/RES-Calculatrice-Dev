d59544b6b426ec14d153ee83f85967e03e77993b
Jordan Mercier <jordan.mercier@heig-vd.ch>
2019-03-28 20:45:05 +0100

    Client is complete

--- src/main/java/client/ClientApplication.java
+++ src/main/java/client/ClientApplication.java
@@ -1,29 +1,20 @@
 package client;
 
-import server.Server;
-
 import java.util.Scanner;
 
 public class ClientApplication {
     public static void main(String[] args) {
         System.setProperty("java.util.logging.SimpleFormatter.format", "%5$s %n");
 
-        Thread listenThread = new Thread(new Server());
-        listenThread.start();
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
         final String IP_SERVER = "localhost";
         final int PORT_SERVER = 3636;
         Client c1 = new Client();
         String computing = "";
-        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Client> ");
+        c1.connect(IP_SERVER, PORT_SERVER);
+        Scanner sc = new Scanner(System.in);
         computing = sc.nextLine();
-        c1.connect("localhost", 3636, computing);
+        c1.sendRequest(computing);
+        c1.disconnect();
     }
 }
