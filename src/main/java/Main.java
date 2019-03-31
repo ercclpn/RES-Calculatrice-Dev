bcc596c73e2c33e734baf038c1bb20f3849b26fa
ercclpn <ercclpn@gmail.com>
2019-03-31 03:02:20 +0200

    Multi-threaded server implemented.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,13 @@
+import server.MultiServer;
 import server.Server;
 
 public class Main {
 
     public static void main(String ... args){
-        Server server = new Server(3636);
+        //Server server = new Server(3636);
 
-        server.serveClients();
+        //server.serveClients();
+        MultiServer multiServer = new MultiServer(3636);
+        multiServer.serveClients();
     }
 }
