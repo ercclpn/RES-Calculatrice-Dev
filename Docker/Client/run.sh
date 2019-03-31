b7af53db5b65ae985e6bf0b8e41f5c173cae4db7
ercclpn <ercclpn@gmail.com>
2019-03-31 16:28:26 +0200

    Change behavior of the client and add docker configuration.

---
+++ Docker/Client/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo docker run -it --rm --name="cal-client" ercclpn/res-cal-client
--- Project/src/main/java/application/ClientApplication.java
+++ Project/src/main/java/application/ClientApplication.java
@@ -1,20 +1,23 @@
-package client;
+package application;
 
+import client.Client;
 import java.util.Scanner;
 
 public class ClientApplication {
-   /* public static void main(String[] args) {
+    public static void main(String[] args) {
         System.setProperty("java.util.logging.SimpleFormatter.format", "%5$s %n");
 
-        final String IP_SERVER = "localhost";
+        String IP_SERVER = "";
         final int PORT_SERVER = 3636;
+        Scanner sc = new Scanner(System.in);
         Client c1 = new Client();
         String computing = "";
-
+        System.out.print("IP address : ");
+        IP_SERVER = sc.nextLine();
         c1.connect(IP_SERVER, PORT_SERVER);
-        Scanner sc = new Scanner(System.in);
+
         computing = sc.nextLine();
         c1.sendRequest(computing);
         c1.disconnect();
-    }*/
+    }
 }
