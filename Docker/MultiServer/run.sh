7952ffed4b1d94020db958643cc5ddaad58a2a49
ercclpn <ercclpn@gmail.com>
2019-03-31 16:00:47 +0200

    Restruct the project and add config for docker.

---
+++ Docker/MultiServer/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo docker run --rm --name="cal-multiserver" ercclpn/res-cal-multiserver
---
+++ Docker/Server/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo docker run --rm --name="cal-server" ercclpn/res-cal-server
